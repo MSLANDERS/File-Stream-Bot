2f64a9a80523f56434816470a873afdec6c39ea8
MSLANDERS <mslanders0@gmail.com>
2025-03-27 19:26:15 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -19,7 +19,7 @@ class Var(object):
     API_HASH = str(getenv('API_HASH', ''))
     BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
     PICS = (environ.get('PICS', 'https://envs.sh/jUp.jpg')).split()
-    name = str(getenv('name', 'ProFilesToLink_Bot'))
+    name = str(getenv('name', 'FilesToLinkPro_Bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002456017788'))
